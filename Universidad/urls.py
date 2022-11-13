c72926aaf13cfa15718f762f928e6828326fa3be
Kaaarlos <ca991467@gmail.com>
2022-11-13 14:32:00 -0600

    ya dios por favooor

--- Universidad/urls.py
+++ Universidad/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.config import settings
-from django.config.urls.statics import static
+from django.config.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Aplicaciones.Academico.urls'))
+    path('',include('Aplicaciones.Academico.urls')),
 ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
